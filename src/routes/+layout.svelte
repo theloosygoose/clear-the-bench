645887ed7b330b09b2e3c93fadf4e2a662933469
theloosygoose <g.ramahenry2439@gmail.com>
2023-04-10 17:06:03 -0400

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-
+  import "../app.css";
 </script>
 
-<h1>Hello from the Layout</h1>
+<h1 class="text-3xl font-bold underline">Hello from the Layout</h1>
+
 <slot />
 
-<style>
+<style lang="postcss">
+
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
 
 </style>
